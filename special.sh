2305703118c29d0a2e2c0ab85d21ebf12c67c956
LOLNOYTE <86774255+LOLNOYTE@users.noreply.github.com>
2024-08-07 14:07:10 +0400

    Update special.sh

--- special.sh
+++ special.sh
@@ -32,15 +32,13 @@ arch-chroot /mnt useradd -m -G wheel -s /bin/bash ${USER}
 arch-chroot /mnt passwd ${USER}
 
 arch-chroot /mnt sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-arch-chroot /mnt su ${USER}
-arch-chroot /mnt sudo pacman -Syu --noconfirm
+arch-chroot /mnt su ${USER} sudo pacman -Syu --noconfirm
 
 arch-chroot /mnt systemctl enable NetworkManager
 arch-chroot /mnt systemctl enable iwd
 arch-chroot /mnt grub-install --target=i386-pc /dev/sda
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt sudo pacman -S plasma sddm konsole firefox dolphin
-exit
 
 
 
